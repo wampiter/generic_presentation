0a3fbe38f1872585eca706f5e528e9c52854142e
Scott R. Johnston <wampiter@gmail.com>
2015-07-09 10:41:00 -0700

    in working state

--- generic_presentation.py
+++ generic_presentation.py
@@ -1,3 +1,22 @@
-head = open("head.tex")
-tail = open("tail.tex")
 
+def gen_pres(scans, slide = "slide_optical_combscan.tex"):
+    head = open("/Users/jot/Desktop/generic_presentation/head.tex", 'r')
+    tail = open("/Users/jot/Desktop/generic_presentation/tail.tex", 'r')
+    slide = open("/Users/jot/Desktop/generic_presentation/slides/" + slide, 'r')
+    output = open("pres.tex", 'w')
+    for line in head:
+        output.write(line)
+    output.write('\n')
+    for number in scans:
+        filename = "scan" + str(number) + "_comb.png"
+        for line in slide:
+            line = line.replace('SCANS', filename)
+            line = line.replace('OPTICAL', 's' + str(number) + '.jpg')
+            line = line.replace('SUBTITLE', '')
+            line = line.replace('TITLE', 'Scan ' + str(number))
+            output.write(line)
+        slide.seek(0)
+        output.write('\n')
+    for line in tail:
+        output.write(line)
+    output.close()
